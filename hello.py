51900bef1db7d1e3e3cb9bc0f6978d2c872f7690
houxinli <houxinli@hotmail.com>
2018-10-16 20:02:50 +0800

    0.1.0 ver

--- hello.py
+++ hello.py
@@ -1,13 +1,84 @@
 from flask import Flask
+# from flask_script import Manager
+from flask import render_template
+from flask import url_for
+import os
+import sys
+# reload(sys) 
+# sys.setdefaultencoding('utf8')
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'Hello, World'
+	return render_template('index.html', songs = All, sname = 'Something_Just_Like_This')
+
+@app.route('/Billboard')
+def Billboard():
+	return render_template('index.html', songs = Billboard, sname = 'Something_Just_Like_This')
+	pass
+
+@app.route('/Personal')
+def Personal():
+	return render_template('index.html', songs = Personal)
+	pass
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return '<h1>SHIT, %s!</h1>' % name
+
+@app.route('/song/Billboard/<sname>/')
+def bsong(sname):
+	return render_template('index.html', songs = Billboard, sname = sname)
+	pass
+
+@app.route('/#!/song/Personal/<sname>/')
+def psong(sname):
+	return render_template('index.html', songs = Personal, sname = sname)
+	pass
+
+class Song(object):
+	"""docstring for Song"""
+	def __init__(self, cat, filename, name):
+		super(Song, self).__init__()
+		self.filename = filename;
+		self.name = name;
+		self.cat = cat;
+		self.mp3 = 'music/' + str(cat) + '/MP3/' + str(filename) + '.mp3'
+		self.cover = 'music/' + str(cat) + '/covers/' + str(filename) + '.png'
+		self.shadow = 'music/' + str(cat) + '/covers/shadow/' + str(filename) + '.jpg'
+		self.lyric = 'music/' + str(cat) + '/lyrics/' + str(filename) + '.txt'
+		
 
 if __name__ == '__main__':
+
+	bpath = 'static/music/Billboard/lyrics/'
+	bsongfiles = os.listdir(bpath)
+	Billboard = {}
+	for songfile in bsongfiles:
+		lyricfile = open(bpath + songfile)
+		name = lyricfile.readline().strip('\n')
+		print(name)
+		lyricfile.close()
+		filename = os.path.splitext(songfile)[0]
+		cursong = Song('Billboard', filename, name)
+		Billboard[name] = cursong
+		pass
+
+	ppath = 'static/music/Personal/lyrics/'
+	psongfiles = os.listdir(ppath)
+	Personal = {}
+	for songfile in psongfiles:
+		lyricfile = open(ppath + songfile ,encoding='gb18030')
+		name = lyricfile.readline().strip('\n')
+		lyricfile.close()
+		filename = os.path.splitext(songfile)[0]
+		cursong = Song('Personal', filename, name)
+		Personal[name] = cursong
+		pass
+
+	All = {**Billboard, **Personal} 
+
 	app.run(debug = True)
+
+	print(Billboard)
---
+++ test.py
@@ -0,0 +1,29 @@
+import os,sys
+
+class Song(object):
+	"""docstring for Song"""
+	def __init__(self, cat, filename, name):
+		super(Song, self).__init__()
+		self.name = name;
+		self.filename = filename;
+		self.cat = cat;
+		self.mp3 = 'static/music/' + str(cat) + '/MP3/' + str(filename) + '.mp3'
+		self.cover = 'static/music/' + str(cat) + '/covers/' + str(filename) + '.png'
+		self.lyric = 'static/music/' + str(cat) + '/lyrics/' + str(filename) + '.txt'
+
+if __name__ == '__main__':
+	billboard = 'static/music/Personal/lyrics/'
+	songfiles = os.listdir(billboard)
+	songs = {}
+	# print(songs[0]- '.mp3')
+	for songfile in songfiles:
+		lyricfile = open(billboard + songfile)
+		name = lyricfile.readline().strip('\n')
+		# print(name)
+		lyricfile.close()
+		songname = os.path.splitext(songfile)[0]
+		cursong = Song('Billboard', songname, name)
+		print(cursong.name)
+		songs[songname] = cursong
+		# print(cursong.cover)
+	# print('sad' + songs['101_Issues'].mp3)
