5e59b9e8a6cd35bd90be7be21c189c0ff68ff740
houxinli <houxinli@hotmail.com>
2018-10-29 02:10:29 +0800

    update

--- bs_learn.py
+++ bs_learn.py
@@ -26,9 +26,9 @@ def getPlaylist(lid = '2256615030'):
     playlist['name'] = lname
     sids = []
     for music in musics:
-        sid = music['href'].strip("/song?id=")
-        sids.append(sid)
-        # snames.append( music.get_text() )
+    	sid = music['href'].strip("/song?id=")
+    	sids.append(sid)
+    	# snames.append( music.get_text() )
     playlist['sids'] = sids
 
     return playlist
@@ -40,19 +40,20 @@ def downloadSong(sid, file_path):
 if __name__ == '__main__':
     # lid = input('input the playlist id:')
     playlists_file = open("static/playlists.json", encoding = 'utf-8')
+    print("file", playlists_file)
     playlists = json.loads(playlists_file)
     lids = ['2256615030']
     file_path = 'music'
     # song_names, song_ids = getPlaylist(lid)
     session = requests.session()
-    for lid in lids
-        if playlists.has_key(lid):
-            pass
-        else :
-            playlist = getPlaylist(lid)
-            playlists['lid'] = playlist
-            for sid in playlist['sids']:
-                downloadSong(sid, file_path)
+    for lid in lids:
+    	if playlists.has_key(lid):
+    		pass
+    	else :
+    		playlist = getPlaylist(lid)
+    		playlists['lid'] = playlist
+    		for sid in playlist['sids']:
+    			downloadSong(sid, file_path)
 
 
     # for i in range(len(song_names)):
@@ -69,17 +70,17 @@ if __name__ == '__main__':
     url = ' http://music.163.com/api/song/detail/?id=569214321&ids=%5B569214321%5D'
     # url = 'http://music.163.com/api/song/lyric?os=pc&id=569214321&lv=-1&kv=-1&tv=-1'
     try:
-        soup = (session.get(url, headers = headers).content , 'lxml' )
-        r = session.get(url, headers = headers)
-        print(r.status_code)
-        detail = json.loads(r.text)
-        print( type(detail) )
-        print( json.dumps(detail, sort_keys=True, indent=4, separators=(',', ': ') ) )
-        # urllib.request.urlretrieve(url, 'GALWAYGIRL.mp3')
-        print("success")
-        print(soup.prettify() )
-        print("ii")
+    	soup = (session.get(url, headers = headers).content , 'lxml' )
+    	r = session.get(url, headers = headers)
+    	print(r.status_code)
+    	detail = json.loads(r.text)
+    	print( type(detail) )
+    	print( json.dumps(detail, sort_keys=True, indent=4, separators=(',', ': ') ) )
+    	# urllib.request.urlretrieve(url, 'GALWAYGIRL.mp3')
+    	print("success")
+    	print(soup.prettify() )
+    	print("ii")
     except Exception as e:
-        if hasattr(e, 'reason'):
-            print("error, reason:", e.reason)
+    	if hasattr(e, 'reason'):
+    		print("error, reason:", e.reason)
 
